b4c5f6b6ac6aea75c3e47b1006ebdb17c4bd0a05
QxBytes <39818795+QxBytes@users.noreply.github.com>
2023-04-16 20:22:15 -0700

    Initial Commit

---
+++ MYND/MYNDApp.swift
@@ -0,0 +1,17 @@
+//
+//  MYNDApp.swift
+//  MYND
+//
+//  Created by Al on 4/16/23.
+//
+
+import SwiftUI
+
+@main
+struct MYNDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
